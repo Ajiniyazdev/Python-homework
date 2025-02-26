c797cafd01c3449ea71d4b235f9e4bb43c951ee0
Ajiniyazdev <84559418+Ajiniyazdev@users.noreply.github.com>
2025-02-26 17:06:48 +0500

    Create Homework2.py

---
+++ lesson-2/Homework/Homework2.py
@@ -0,0 +1,19 @@
+# Problem 1
+# import datetime
+# name = input('Your name is: ')
+# year = int(input('Say me your year of birth: '))
+# age = datetime.datetime.now().year - year
+# print(f'Hi {name}, you age is {age}')
+
+# Problem 2
+cars = 'LMaasleitbtui'
+print(cars[1::2])
+print(cars[::2])
+# Problem 3
+txt = 'MsaatmiazD'
+print(txt[::2])
+print(txt[-1::-2])
+# Problem 4
+city = 'I\'m John. I\'m from London'
+city = city.split('from')
+print(city)
